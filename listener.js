0af06d69481c5f3ecb1cc766aae89fb673f27489
mmargegaj <mario.margegaj@gmail.com>
2023-09-09 21:45:37 +0200

    Update

--- listener.js
+++ listener.js
@@ -1,36 +1,35 @@
 const logger = require("node-color-log");
 const io = require("socket.io-client");
 
-(() => {
-  const socket = io(`http://${process.argv[2]}:3000`);
-  setInterval(() => null, 30000);
-  socket.on("eqInfo", (data) => {
-    logger.info(data);
-  });
+const socket = io(`http://localhost:3002`);
 
-  socket.on("connection", () => {
-    logger.info("connected");
-  });
+socket.on("eqInfo", (data) => {
+  logger.info(data);
+});
 
-  socket.on("disconnect", (reason) => {
-    logger.warn(reason);
-    if (reason === "io server disconnect") {
-      // the disconnection was initiated by the server, you need to reconnect manually
-      socket.connect();
-    } else if (reason === "transport close"){
-      logger.error("Server stopped");
-    }
-    // else the socket will automatically try to reconnect
-  });
+socket.on("connection", () => {
+  logger.info("connected");
+});
 
-  socket.on("predictionInfo", (data) => {
-    logger.info(data);
-  });
+socket.on("disconnect", (reason) => {
+  logger.warn(reason);
+  if (reason === "io server disconnect") {
+    // the disconnection was initiated by the server, you need to reconnect manually
+    socket.connect();
+  } else if (reason === "transport close") {
+    logger.error("Server died");
+  }
+  // else the socket will automatically try to reconnect
+});
 
-  socket.on("new_earthquakes", (data) => {
-    logger.info(data);
-  });
-  socket.on("new_predictions", (data) => {
-    logger.info(data);
-  });
-})();
+socket.on("predictionInfo", (data) => {
+  logger.info(data);
+});
+
+socket.on("new_earthquakes", (data) => {
+  logger.info(data);
+});
+
+socket.on("predictions", (data) => {
+  logger.info(data);
+});
